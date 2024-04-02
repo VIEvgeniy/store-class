1c46aed19df0f59098c4eb251d3a42224fbba9f7
Evgeniy Vakulenko <vakevg@yandex.ru>
2024-04-02 16:56:39 +0300

    Changes to be committed: 	modified:   src/component/ShopItemClass.jsx

--- src/component/ShopItemClass.jsx
+++ src/component/ShopItemClass.jsx
@@ -1,6 +1,6 @@
-import PropTypes from 'prop-types'
+// import PropTypes from 'prop-types'
 
-const ShopItemFunc = ({item}) => (
+const ShopItemClass = ({item}) => (
     <div className="main-content">
     <h2>{item.brand}</h2>
     <h1>{item.title}</h1>
@@ -15,17 +15,36 @@ const ShopItemFunc = ({item}) => (
 </div>    
 )
 
-ShopItemFunc.propTypes = {
-    item: PropTypes.shape(
-        {
-        brand: PropTypes.string,
-        title: PropTypes.string,
-        description: PropTypes.string,
-        descriptionFull: PropTypes.string,
-        price: PropTypes.number,
-        currency: PropTypes.oneOf(['₽','£','$','€']),
-        }
-    )
-}
+// class ShopItemClass extends React.Component {
+//     render() {
+//         return (
+//             <div className="main-content">
+//             <h2>{this.props.item.brand}</h2>
+//             <h1>{this.props.item.title}</h1>
+//             <h3>{this.props.item.description}</h3>
+//             <div className="description">{this.props.item.descriptionFull}</div>
+//             <div className="highlight-window mobile"><div className="highlight-overlay"></div></div>
+//             <div className="divider"></div>
+//             <div className="purchase-info">
+//             <div className="price">{this.props.item.currency}{this.props.item.price.toFixed(2)}</div>
+//             <button>Добавить в корзину</button>
+//             </div>
+//         </div>    
+//         )
+//     }
+// }
 
-export default ShopItemFunc
+// ShopItemFunc.propTypes = {
+//     item: PropTypes.shape(
+//         {
+//         brand: PropTypes.string,
+//         title: PropTypes.string,
+//         description: PropTypes.string,
+//         descriptionFull: PropTypes.string,
+//         price: PropTypes.number,
+//         currency: PropTypes.oneOf(['₽','£','$','€']),
+//         }
+//     )
+// }
+
+export default ShopItemClass
